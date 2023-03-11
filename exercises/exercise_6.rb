90716a1e7fdc5b08c37ae87c2c6d57abf5aecafe
tronross <john_ross@analogforever.ca>
2023-03-11 18:56:35 -0500

    Add @store4, @store5; add employees (.create)

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+
+@store1.employees.create(first_name: "Steve", last_name: "Jones", hourly_rate: 60)
+@store1.employees.create(first_name: "Nancy", last_name: "Bogoch", hourly_rate: 160)
+@store2.employees.create(first_name: "Roberta", last_name: "Johannes", hourly_rate: 60)
+@store2.employees.create(first_name: "Ali", last_name: "DeSantes", hourly_rate: 160)
+@store4.employees.create(first_name: "Roberta", last_name: "Johannes", hourly_rate: 60)
+@store4.employees.create(first_name: "Barlo", last_name: "vonStinkalot", hourly_rate: 1600)
+@store5.employees.create(first_name: "Jay", last_name: "DiPhillipo", hourly_rate: 60)
+@store5.employees.create(first_name: "Asha", last_name: "Morales", hourly_rate: 160)
