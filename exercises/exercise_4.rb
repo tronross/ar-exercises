795147158adb1492878c43ff2a648563239c14ff
tronross <john_ross@analogforever.ca>
2023-03-11 18:12:58 -0500

    Add logic to add three stores; select and print menswear stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,29 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create( 
+                      name: "Surrey",
+                      annual_revenue: 224000,
+                      womens_apparel: true
+                    )
+
+store = Store.create( 
+                      name: "Whistler",
+                      annual_revenue: 1900000,
+                      mens_apparel: true
+                    )
+
+store = Store.create(
+                      name: "Yaletown",
+                      annual_revenue: 190000,
+                      mens_apparel: true
+                    )
+
+puts "There are #{Store.count} stores."
+
+
+@mens_stores = Store.where("mens_apparel = true")
+
+@mens_stores.find_each do |store| 
+  puts "Store: #{store.name}      Annual Revenue: #{store.annual_revenue}"
+end
