ae2547cc9e11b97416a48be98f514a0e33fc32d7
tronross <john_ross@analogforever.ca>
2023-03-11 18:40:41 -0500

    Add logic for calculations (sum, average, and >=)

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@company_revenue = Store.sum("annual_revenue")
+@average_revenue = Store.average("annual_revenue")
+@stores_over_1M = Store.where("annual_revenue >= 1000000").count
+
+puts "Company Annual Revenue: #{@company_revenue}"
+puts "Average Annual Revenue per store: #{@average_revenue}"
+puts "There are #{@stores_over_1M} stores with an annual revenue over $1,000,000"
