075c5a61fcaa2357db55fddb1880463796c03ac2
tronross <john_ross@analogforever.ca>
2023-03-12 13:18:33 -0400

    Add custom validation :must_carry_either_mens_or_womens_apparel

--- lib/store.rb
+++ lib/store.rb
@@ -2,11 +2,11 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3}
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
-  # validates :must_carry_either_mens_or_womens_apparel
+  validate :must_carry_either_mens_or_womens_apparel
 
-  # def must_carry_either_mens_or_womens_apparel
-  #   if !(mens_apparel.present? || womens_apparel.present?)
-  #     errors.add(:mens_apparel, "Must sell either men's apparel or women's apparel.")
-  #   end
-  # end
+  def must_carry_either_mens_or_womens_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, "Must sell either men's apparel or women's apparel.")
+    end
+  end
 end
