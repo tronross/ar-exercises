80aca677373c119bd8afc3f46adb2f15054556e5
tronross <john_ross@analogforever.ca>
2023-03-12 13:25:15 -0400

    Lint validates :name

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validates :name, length: { minimum: 3}
+  validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
   validate :must_carry_either_mens_or_womens_apparel
 
